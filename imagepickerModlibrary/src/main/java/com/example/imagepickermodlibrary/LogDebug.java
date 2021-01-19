09723fc7e5664e28cf7903650e9e9693b93715d8
liendo1 <panaliendo@outlook.com>
2021-01-19 10:13:08 +0100

    Initial commit

---
+++ imagepickerModlibrary/src/main/java/com/example/imagepickermodlibrary/LogDebug.java
@@ -0,0 +1,9 @@
+package com.example.imagepickermodlibrary;
+import android.util.Log;
+public class LogDebug {
+
+    private static final String TAG = "SUPER AWESOME";
+    public static void d(String message){
+        Log.d(TAG,message);
+    }
+}
